b4c30b9757ce1f9591db96f588d8db59d0d4bd2a
trungnn <trung.nn@intelin.vn>
2023-05-05 17:54:20 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,220 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
+import avatar from "./assets/__avatar_url.png";
+import senderAvatar from "./assets/senderAvatar.png";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Question from "./components/Question/question";
+import { Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 function App() {
-  const listQuestions = [
-    {
-      id: 1,
-      questionName: "What is React?",
-      answer: 2,
-      listChooses: [
+  const [dataConvertation, setDataConvertation] = useState({
+    conversation: {
+      participants: [
         {
-          id: 1,
-          content: "A JavaScript library for building user interfaces Sai",
+          name: "John",
+          avatar: "https://example.com/avatar1.png",
         },
         {
-          id: 2,
-          content: "A JavaScript library for building user interfaces Đúng",
+          name: "Jane",
+          avatar: "https://example.com/avatar2.png",
+        },
+      ],
+      messages: [
+        {
+          id: "1",
+          sender: {
+            name: "John",
+            avatar: "https://example.com/avatar1.png",
+          },
+          content: "Hi Jane, how are you doing?",
+          timestamp: "2023-05-05T10:15:00Z",
         },
         {
-          id: 3,
-          content: "A JavaScript library for building user interfaces Sai",
+          id: "2",
+          sender: {
+            name: "Jane",
+            avatar: "https://example.com/avatar2.png",
+          },
+          content: "Hey John, I'm good. How about you?",
+          timestamp: "2023-05-05T10:16:00Z",
+        },
+        {
+          id: "3",
+          sender: {
+            name: "John",
+            avatar: "https://example.com/avatar1.png",
+          },
+          content: "I'm doing well too. Thanks for asking.",
+          timestamp: "2023-05-05T10:17:00Z",
+        },
+        {
+          id: "4",
+          sender: {
+            name: "Jane",
+            avatar: "https://example.com/avatar2.png",
+          },
+          content: "Great to hear!",
+          timestamp: "2023-05-05T10:18:00Z",
+        },
+        {
+          id: "5",
+          sender: {
+            name: "John",
+            avatar: "https://example.com/avatar1.png",
+          },
+          content: "I'm doing well too. Thanks for asking.",
+          timestamp: "2023-05-05T10:17:00Z",
+        },
+        {
+          id: "6",
+          sender: {
+            name: "Jane",
+            avatar: "https://example.com/avatar2.png",
+          },
+          content: "Great to hear!",
+          timestamp: "2023-05-05T10:18:00Z",
+        },
+        {
+          id: "7",
+          sender: {
+            name: "John",
+            avatar: "https://example.com/avatar1.png",
+          },
+          content: "I'm doing well too. Thanks for asking.",
+          timestamp: "2023-05-05T10:17:00Z",
+        },
+        {
+          id: "8",
+          sender: {
+            name: "John",
+            avatar: "https://example.com/avatar2.png",
+          },
+          content: "Great to hear!",
+          timestamp: "2023-05-05T10:18:00Z",
         },
       ],
     },
-    {
-      id: 2,
-      questionName: "What is Vite?",
-      answer: 3,
-      listChooses: [
-        {
-          id: 1,
-          content: "A JavaScript library for building user interfaces Sai",
-        },
-        {
-          id: 2,
-          content: "A JavaScript library for building user interfaces Sai",
-        },
-        {
-          id: 3,
-          content: "A JavaScript library for building user interfaces Đúng",
-        },
-      ],
-    },
-  ];
+  });
 
-  const [listAnwser, setListAnwser] = useState();
+  // hook form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({});
 
-  const handleSubmit = () => {
-    // Kiểm tra đáp án trong dataAnswer có trùng với đáp án trong listAnwser không
-    // Nếu trùng thì đúng, không trùng thì sai
-    // Tính điểm
-    setListAnwser([
-      [
-        {
-          questionId: 1,
-          correctAnswer: 2,
-        },
-        {
-          questionId: 2,
-          correctAnswer: 3,
-        },
-      ],
-    ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const onSubmit = (data) => {
+    const newData = {
+      id: dataConvertation.conversation.messages.length + 1,
+      sender: {
+        name: "John",
+        avatar: "https://example.com/avatar1.png",
+      },
+      content: data.content,
+      timestamp: "2023-05-05T10:18:00Z",
+    };
+    setDataConvertation({
+      conversation: {
+        ...dataConvertation.conversation,
+        messages: [...dataConvertation.conversation.messages, newData],
+      },
+    });
+
+    // Tạo hiệu ứng loading 3s
+    setIsTyping(true);
+
+    setTimeout(() => {
+      setIsTyping(false);
+    }, 5000);
+    reset();
   };
+  const dummy = useRef();
 
-  const [dataAnswer, setDataAnswer] = useState({});
-
-  console.log(dataAnswer);
-
-  const [activeQuestion, setActiveQuestion] = useState(1);
-
+  useEffect(() => {
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  }, [dataConvertation]);
   return (
-    <div className="App">
-      <div className="container">
-        <div className="d-flex flex-column justify-content-between align-items-center">
-          {listQuestions.map((question) => {
-            return (
-              <div key={question.id}>
-                <Question
-                  question={question}
-                  dataAnswer={dataAnswer}
-                  setDataAnswer={setDataAnswer}
-                />
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{
+        height: "100vh",
+      }}
+    >
+      <div className="chat__container">
+        <div className="chat__room">
+          <div className="chat__header">
+            <div className="chat__header__left">
+              <img
+                src={reactLogo}
+                alt="react logo"
+                className="sender__avatar"
+              />
+              <dir>
+                <h4>BOT</h4>
+                <p className="status__active">active</p>
+              </dir>
+            </div>
+
+            <div className="chat__header__right">
+              <i className="bi bi-three-dots"></i>
+            </div>
+          </div>
+          <div className="chat__content">
+            {dataConvertation.conversation.messages.map((message) => (
+              <div
+                key={message.id}
+                className={`chat__message ${
+                  message.sender.name === "John"
+                    ? "chat__message__right"
+                    : "chat__message__left"
+                }`}
+              >
+                <div className="chat__message__content">
+                  <p>{message.content}</p>
+                  <span className="chat__timestamp">{message.timestamp}</span>
+                </div>
+                <div className="chat__message__avatar">
+                  <img
+                    src={message.sender.name === "John" ? avatar : senderAvatar}
+                    alt="sender avatar"
+                    className="chat__message__avatar"
+                  />
+                </div>
               </div>
-            );
-          })}
-          <div className="t">
-            <button
-              className="btn btn-primary"
-              onClick={() => {
-                setActiveQuestion(activeQuestion + 1);
-              }}
-            >
-              Next
-            </button>
-            <button
-              className="btn btn-primary"
-              onClick={() => {
-                setActiveQuestion(activeQuestion - 1);
-              }}
-            >
-              Prev
-            </button>
+            ))}
+
+            {
+              // Hiệu ứng loading
+              isTyping && (
+                <div className="typing__animation">
+                  <div className="typing__dot"></div>
+                  <div className="typing__dot"></div>
+                  <div className="typing__dot"></div>
+                </div>
+              )
+            }
+
+            <span ref={dummy}></span>
+          </div>
+
+          <div className="chat__footer">
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <div className="input__sendMessage">
+                <textarea
+                  type="text"
+                  placeholder="Hãy nhập nội dung"
+                  {...register("content", { required: true })}
+                />
+                <i onClick={handleSubmit(onSubmit)} className="bi bi-send"></i>
+              </div>
+              {errors.content && (
+                <span className="error__message">Vui lòng nhập nội dung</span>
+              )}
+            </Form>
           </div>
         </div>
       </div>
